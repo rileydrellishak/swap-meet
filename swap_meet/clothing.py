e109001ccfb6b3b4f17cb5609ac907e64930bec4
Riley Drellishak <rileydrellishak@gmail.com>
2025-09-30 16:57:40 -0400

    implements override str method for Clothing class, removes in-line comments for item.py

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -5,3 +5,7 @@ class Clothing(Item):
     def __init__(self, id=None, fabric="Unknown"):
         super().__init__(id)
         self.fabric = fabric
+        
+    def __str__(self):
+        return (f"An object of type Clothing with id {self.id}. "
+        f"It is made from {self.fabric} fabric.")
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -10,7 +10,3 @@ class Item:
     
     def __str__(self):
         return f"An object of type Item with id {self.id}."
-
-# def get_by_id(self, id):
-# if the id given matches the id of an item in inventory, return the item
-# if id given is not an item in inventory, return None
