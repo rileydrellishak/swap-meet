40d86f6c3f8bde9fa3ea7234242049a1a740d939
geethavs88 <geethavs88@gmail.com>
2025-09-30 13:13:23 -0700

    wave i code updated

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,37 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = [] if inventory is None else inventory
+
+    def add(self, item):
+        '''
+        This method add an item to the inventory list.
+        Input: item to be added to inventory
+        Output: item added to the inventory
+
+        '''
+        self.inventory.append(item)
+        return item
+    def remove(self, item):
+        '''
+        This method removes an item from the inventory list.
+        Input: item to be removed from the inventory.
+        Output: 
+        '''
+        for position in range(len(self.inventory)):
+            if self.inventory[position] == item:
+                temp_variable = item
+                self.inventory[position] = self.inventory[-1]
+                self.inventory[-1] = item
+                item_popped = self.inventory.pop(-1)
+                return item_popped
+            else:
+                return False
+
+        # if item in self.inventory:
+        #     self.inventory.remove(item)
+        #     return item
+        # else:
+        #     return False   
+
+
+
