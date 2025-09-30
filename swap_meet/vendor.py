4ba6d96fa890ac709fbb19aa51e7a4053394ef61
geethavs88 <geethavs88@gmail.com>
2025-09-30 14:10:36 -0700

    updated swap method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -26,15 +26,28 @@ class Vendor:
                 return item_popped
         return False
 
-    def str(self)
     def get_by_id(self, id):
         for item in self.inventory:
             if item.id == id:
                 return item
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        self.add(other_vendor.remove(their_item))
+        other_vendor.add(self.remove(my_item))
+        return True
+
+
+
+
     def swap_first_item(self, other_vendor):
         
-        item 
+        temp_item = self.remove(self.inventory[0])
+        self.inventor
+        self
+
         temp_item = self.inventory[0]
         self.inventory[0] = other_vendor.invetory[0]
         other_vendor.inventory[0] = temp_item
