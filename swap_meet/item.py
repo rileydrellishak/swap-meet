cee463e1890e23eb7037552c341a0d09bb2be3c3
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-01 09:01:21 -0400

    Adds docstrings to Item and Vendor class methods

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -7,12 +7,26 @@ class Item:
         self.condition = condition
 
     def get_category(self):
+        '''Returns a string of the name of the class.
+        Input: None (must be called on an instance of Item).
+        Output: a string literal representing the name of the class.
+        '''
         return "Item"
     
     def __str__(self):
+        '''
+        Stringify an Item using str()
+        Input: None (must be called on an instance of Item).
+        Output: a string literal of a description of the item type and id.
+        '''
         return f"An object of type {self.get_category()} with id {self.id}."
     
     def condition_description(self):
+        '''
+        Returns a string literal describing the condition of the item.
+        Input: None (must be called on an instance of Item).
+        Output: a string literal representing a description of the condition on a scale from 0 to 5, 5 being the best condition.
+        '''
         if self.condition >= 0 and self.condition < 1:
             return "Horrible!"
         elif self.condition >= 1 and self.condition < 2:
